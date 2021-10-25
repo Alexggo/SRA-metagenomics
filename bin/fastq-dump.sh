e87b68d7afebfd7eeed660cda094ab8adca1ece0
Alex G <alegigo@gmail.com>
2021-10-25 17:16:50 -0400

    new code

--- bin/fastq-dump.sh
+++ bin/fastq-dump.sh
@@ -7,17 +7,26 @@
 #SBATCH --time=04:00:00
 
 ### SET WD
-cd /gpfs/projects/RestGroup/alex/ch4_rnaseq/data/Videvall2018
+cd ~/Projects
 
 ### LOAD MODULES
-module load shared
-module load git
-module load anaconda/2
+#module load shared
+#module load git
+#module load anaconda/2
 
 ### LOAD ENVIRONMENT
-source activate my_env
+conda activate bioinformatics
 
 ### SCRIPT
 
-less SRR_Acc_List.txt | while read line; do fastq-dump $line; done
-mv *.fastq ../results/Videvall2018/SRA
+less ch4-c_auris/data/bird_metagenome/SRR_Acc_List.txt |head -n 5| while read line
+do 
+	fastq-dump --split-files $line && mv $line_1.fastq SRR
+	# Map sequences to the DB with kma
+	#kma -i $line_1.fasta -t_db $CCM_DB -t 24 -mem_mode -and -o $line_1_kma_out
+	# Run CCMetagen
+	#CCMetagen.py -i SRR2568530_1_kma_out.res -o SRR2568530_results
+done
+rm *.fastq
+
+echo "all done!"
