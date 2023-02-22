cacf4ab29956e11f45f6d32c00f3095efd42e64c
Alexggo <alegigo@gmail.com>
2023-02-21 23:50:45 -0500

    script modified

--- bin/00.runCCmetagen.sh
+++ bin/00.runCCmetagen.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 #SBATCH --nodes=1
-#SBATCH --partition=extended-40core
+#SBATCH --partition=extended-28core
 #SBATCH --time=7-00:00:00
-#SBATCH --ntasks-per-node=24
+#SBATCH --ntasks-per-node=28
 
 #RUN WITH:
 #sbatch --export=top=15,last=15,taxa=mammalsubset,seq=all --job-name=$taxa.r --output=$taxa.$top.$last.out.txt bin/00.find_script_allatonce.sh
 
 #top=500
 #last=500
-#taxa=mammalsubset
+#taxa=bombus
 #seq=all
-#sbatch --export=top=3,last=3,taxa=test,seq=all --job-name=$taxa.r --output=$taxa.$top.$last.out.txt script.sh
+# sbatch --export=top=15,last=15,taxa=bombus,seq=all --job-name=$taxa.r --output=$taxa.$top.$last.out.txt bin/00.runCCmetagen.sh
 
 ### LOAD MODULES
 #module load shared
@@ -25,22 +25,21 @@ module load gnu-parallel/6.0
 from=$(expr $top - $last + 1)
 to=$top
 threads=40
-echo $from
-echo $to
+echo Range: $from-$to
 
 #Set path to database
-nt_db=/gpfs/software/blastDBs/nt/ncbi_nt_no_env_11jun2019
+nt_db=/gpfs/software/blastDBs/refseq_nt/refSeq_viral_fungi.genomic.fna.gz
 # For nt_no_env use:
 # /gpfs/software/blastDBs/nt/ncbi_nt_no_env_11jun2019
 # For updated db, but only with virus/fungi sample use: 
 # /gpfs/software/blastDBs/refseq_nt/refSeq_viral_fungi.genomic.fna.gz
-accession_list=/gpfs/projects/RestGroup/agilgomez/projects/ch4-c_auris/data/SraAccList_${taxa}_${seq}.txt
+accession_list=/gpfs/projects/RestGroup/agilgomez/projects/ch4-metagenomics/data/bees/SraAccList_${taxa}_${seq}.txt
 
 #Directories
 scratch_dir=/gpfs/scratch/agilgomez/Databases/SRR
-KMA_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-c_auris/results/01_KMA_res
-CC_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-c_auris/results/02_CCMetagen
-Table_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-c_auris/results/03_Output_Tables
+KMA_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-metagenomics/results/01_KMA_res
+CC_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-metagenomics/results/02_CCMetagen
+Table_dir=/gpfs/projects/RestGroup/agilgomez/projects/ch4-metagenomics/results/03_Output_Tables/bees
 output_table=SRAtab_${taxa}_${seq}_${from}-${to}
 subdir_name="SRA_${taxa}_${seq}_${from}-${to}"
 
@@ -71,74 +70,47 @@ mkdir ${CC_dir}
 mkdir ${CC_dir}/${subdir_name}
 mkdir /gpfs/scratch/agilgomez/temp/${subdir_name}
 
+head $accession_list -n $top | tail -n $last > ${scratch_dir}/${subdir_name}/filtered_list.txt
+head ${scratch_dir}/${subdir_name}/filtered_list.txt
 
-cat $accession_list | head -n $top | tail -n $last > ${scratch_dir}/${subdir_name}/filtered_list.txt
-cat ${scratch_dir}/${subdir_name}/filtered_list.txt | head
+while read accession; do
+    echo "Processing accession: $accession"
 
-less ${scratch_dir}/${subdir_name}/filtered_list.txt| while read line
-do 
-	echo "Downloading:" $line
-	fasterq-dump --outdir ${scratch_dir}/${subdir_name} --mem 1G --split-3 --threads $threads --include-technical -S  --print-read-nr $line -t /gpfs/scratch/agilgomez/temp/${subdir_name}
+    # Download the file using fasterq-dump
+    fasterq-dump --split-3 "$accession" --outdir ${scratch_dir}/${subdir_name} --mem 1G --threads $threads --include-technical -S  -t /gpfs/scratch/agilgomez/temp/${subdir_name}
 	rm /gpfs/scratch/agilgomez/temp/${subdir_name}/* -rf
-	find $scratch_dir/$subdir_name -type f -name "${line}*" | while read r
-	do
-		echo "KMA for:" $r
-		if [[ "$r" == *"_1.fastq"* ]];then
-			echo "File ends in _1.fastq"
-  			r1=$r
-  			r2=${r1/_1.fastq/_2.fastq}
-  			if [[ -f "$r2" ]];then
-  				echo "There is a _2. Doing paired"
-  				echo "R1 is:" $r1
-  				echo "R2 is:" $r2
-  				o=$KMA_dir/${r1/$scratch_dir\//''}
-  				echo "Output directory:" $o
-	    		kma -ipe $r1 $r2 -o $o -t_db $nt_db -t $threads -1t1 -mem_mode -and -apm f
-				rm $r2
-				rm $r1
-  			else
-  				echo "R1 is:" $r1
-    			echo "There is not a _2. Doing single"
-    			o=$KMA_dir/${r1/$scratch_dir\//''}
-				echo $o
-				kma -i $r -o $o -t_db $nt_db -t $threads -1t1 -mem_mode -and
-				rm $r1
-			fi
-  		else
-  			echo "It doesn't end in _1.fastq"
-  			if [[ "$r" == *"_2.fastq"* ]];then
-  				r2=$r
-  				echo "R2 is:" $r2
-  				echo "File ends in _2. Doing nothing."
-  			else
-  				echo "File is named" $r
-  				echo "File ends in only in .fastq, but not _2 or _1. Doing single"
-  				o=$KMA_dir/${r/$scratch_dir\//''}
-				echo $o
-				kma -i $r -o $o -t_db $nt_db -t $threads -1t1 -mem_mode -and
-				rm $r
-			fi
-		fi
-	done
-	rm /gpfs/scratch/agilgomez/temp/${subdir_name} -rf
-	rm ${scratch_dir}/${subdir_name} -rf
-	mkdir ${scratch_dir}/${subdir_name}
+	echo "${scratch_dir}/${subdir_name}/${accession}_1.fastq"
+	o=$KMA_dir/${subdir_name}/${accession}.fastq
+	echo "Output directory:" $o
+    # Process the downloaded files
+    if [[ -f "${scratch_dir}/${subdir_name}/${accession}_1.fastq" && -f "${scratch_dir}/${subdir_name}/${accession}_2.fastq" ]]; then
+        # Files are paired-end
+        echo "Processing paired-end files for $accession" 
+        kma -ipe "${scratch_dir}/${subdir_name}/${accession}_1.fastq" "${scratch_dir}/${subdir_name}/${accession}_2.fastq" -o $o -t_db $nt_db -t $threads -1t1 -mem_mode -and -apm f
+    	#Some files have 0MB. Remove empty .res files from analysis.
+		find ${KMA_dir}/${subdir_name}/*.res -size  0 -print -delete
+		echo File: ${KMA_dir}/${subdir_name}/${accession}.fastq.res
+		CCMetagen.py -i "${KMA_dir}/${subdir_name}/${accession}.fastq.res" -o ${CC_dir}/${subdir_name}
+		rm "${KMA_dir}/${subdir_name}/${accession}*"
+		echo "Fastq and kma steps completed"
+		ls ${CC_dir}/${subdir_name} -ltrh
 
-	
-	#Some files have 0MB. Remove empty .res files from analysis.
-	find ${KMA_dir}/${subdir_name}/*.res -size  0 -print -delete
+	elif [[ -f "${scratch_dir}/${subdir_name}/${accession}.fastq" ]]; then
+        # File is single-end
+        echo "Processing single-end file for $accession"
+        kma -i $r -o $o -t_db $nt_db -t $threads -1t1 -mem_mode -and
+		#Some files have 0MB. Remove empty .res files from analysis.
+		find ${KMA_dir}/${subdir_name}/*.res -size  0 -print -delete
+		CCMetagen.py -i ${KMA_dir}/${subdir_name}/*.res -o ${CC_dir}/${subdir_name}
+		rm "${KMA_dir}/${subdir_name}/${accession}*"
+		echo "Fastq and kma steps completed"
+		ls ${CC_dir}/${subdir_name} -ltrh
+    else
+        # Files not found
+        echo "Error: Failed to find files for $accession"
+    fi
 
-	for f in ${KMA_dir}/${subdir_name}/*.res; do 
-		echo "CCMetagen for:" $f
-		out=${CC_dir}/${f/$KMA_dir\/}
-		echo $out
-		CCMetagen.py -i $f -o $out
-	done
-	rm ${KMA_dir}/${subdir_name} -rf
-	mkdir ${KMA_dir}/${subdir_name}
-done
-echo "Fastq and kma steps completed"
-ls ${CC_dir}/${subdir_name} -ltrh
+done < ${scratch_dir}/${subdir_name}/filtered_list.txt
 
 echo "CCMetagen merge"
 CCMetagen_merge.py --input_fp ${CC_dir}/${subdir_name}  --output_fp ${Table_dir}/$output_table
@@ -153,5 +125,3 @@ rm ${CC_dir}/${subdir_name} -rf
 
 echo "Finishing script"
 date +"%T"
-
-
